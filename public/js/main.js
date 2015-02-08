9a0ca40ed7b4d314234709d460ec2900c1fbd32a
Brian <iamamthwiz+GitHub@gmail.com>
2015-02-07 17:45:41 -0800

    connect site to server to display output

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+(function (root) {
+    var output = document.getElementById("output");
+    var socket = io.connect("localhost:8000/web");
+    
+    socket.on("connection", function () {
+        console.log("socket connected");
+    });
+    
+    socket.on("output", function (data) {
+        var pre = document.createElement("pre");
+        var line = data.line;
+        var textNode = document.createTextNode(line);
+        
+        console.log(line);
+        
+        pre.appendChild(textNode);
+        output.appendChild(pre);
+    });
+})(this);
