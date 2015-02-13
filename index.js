5cf26a7c9294612160b68776524ef91679edd421
Brian <iamamthwiz+GitHub@gmail.com>
2015-02-12 22:19:05 -0800

    move sockets to its own file

--- index.js
+++ index.js
@@ -47,24 +47,4 @@ server.listen(PORT, function () {
     console.log("server running on PORT: " + PORT);
 });
 
-var terminal = io.of("/terminal").on("connection", function (socket) {
-    socket.on("command", function (command) {
-        web.emit("command", {
-            "command": command
-        });
-    });
-    
-    socket.on("terminal-output", function (text) {
-        web.emit("output", {
-            "line": text
-        });
-    });
-    
-    socket.on("terminal-error", function (text) {
-        web.emit("error", {
-            "line": text
-        });
-    });
-});
-
-var web = io.of("/web");
+require("./app/sockets.js")(io);
