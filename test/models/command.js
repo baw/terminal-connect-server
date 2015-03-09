a5b3333318e86aa4470cf8376fef024d45f259ba
Brian <iamamthwiz+GitHub@gmail.com>
2015-03-08 23:50:51 -0700

    add test for command schema

---
+++ test/models/command.js
@@ -0,0 +1,89 @@
+var TEST_DB_URL = "mongodb://localhost/tc-test";
+
+var chai = require("chai");
+var charDateTime = require("chai-datetime");
+var mongoose = require("mongoose");
+var mochaMongoose = require("mocha-mongoose")(TEST_DB_URL);
+
+chai.use(charDateTime);
+var expect = chai.expect;
+
+var Command = require("../../app/models/command.js");
+var User = require("../../app/models/user.js");
+
+describe("Command model", function () {
+    var commandId;
+    var commandData = {
+        lines: [],
+        name: "name",
+        public: true
+    };
+    
+    beforeEach(function (done) {
+        if (mongoose.connection.db) {
+            done(null);
+        } else {
+            mongoose.connect(TEST_DB_URL, done);
+        }
+    });
+    
+    beforeEach(function (done) {
+        var u = new User();
+        
+        c = new Command(commandData);
+        c.user = u.id;
+        c.save();
+        
+        commandId = c.id;
+        
+        done();
+    });
+    
+    describe("lines attribute", function () {
+        it("should exist", function (done) {
+            Command.findById(commandId, function (err, command) {
+                if (err) throw err;
+                
+                expect(command.lines).to.not.be.undefined;
+                
+                done();
+            });
+        });
+    });
+    
+    describe("name attribute", function () {
+        it("should exist", function (done) {
+            Command.findById(commandId, function (err, command) {
+                if (err) throw err;
+                
+                expect(command.name).to.not.be.undefined;
+                
+                done();
+            });
+        });
+    });
+    
+    describe("public attribute", function () {
+        it("should exist", function (done) {
+            Command.findById(commandId, function (err, command) {
+                if (err) throw err;
+                
+                expect(command.public).to.not.be.undefined;
+                
+                done();
+            });
+        });
+    });
+    
+    describe("user attribute", function () {
+        it("should exist", function (done) {
+            Command.findById(commandId, function (err, command) {
+                if (err) throw err;
+                
+                expect(command.user).to.not.be.undefined;
+                
+                done();
+            });
+        });
+    });
+});
