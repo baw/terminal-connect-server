071b186925f97543026be4b4f76ac9a6edd39221
Brian Weiser <iamamthwiz+GitHub@gmail.com>
2015-04-19 00:49:55 -0700

    remove container reference

--- public/js/main.jsx
+++ public/js/main.jsx
@@ -3,8 +3,6 @@ var Router = require("react-router");
 
 var routes = require("./routes.jsx");
 
-var Container = require("./components/container.jsx");
-
 Router.run(routes, function (Handler) {
     React.render(<Handler />, document.body);
 });
